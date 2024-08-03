3385e5b31ce21fc0abcc7108b61e38cd7e38a668
Andrew Steurer <andrew.steurer@fermyon.com>
2024-08-02 19:36:52 -0600

    updating help messages and removing windows + arm64 from release
    Signed-off-by: Andrew Steurer <andrew.steurer@fermyon.com>

--- cmd/up.go
+++ cmd/up.go
@@ -10,7 +10,8 @@ import (
 
 var upCmd = &cobra.Command{
 	Use:   "up",
-	Short: "Runs a Spin App with the default OTel environment variables. Flags for the 'spin up' command can be used as well: 'spin otel up -- --help'",
+	Short: "Runs a Spin App with the default OTel environment variables.",
+	Long:  "Runs a Spin App with the default OTel environment variables. Any flags that work with the `spin up` command, will work with the `spin otel up` command: 'spin otel up -- --help'",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := up(args); err != nil {
 			return err
